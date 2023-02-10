a23914b61399267337a145b0f9ab9cd104d189ad
Wilson <wilsonla0920@gmail.com>
2023-02-10 18:17:13 -0500

    Set up application.properties and implemented the book entity

---
+++ src/main/java/com/luv2code/springbootlibrary/entity/Book.java
@@ -0,0 +1,34 @@
+package com.luv2code.springbootlibrary.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "book")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "author")
+    private String author;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "copies")
+    private int copies;
+    @Column(name = "copies_available")
+    private int copiesAvailable;
+    @Column(name = "category")
+    private String category;
+    @Column(name = "img")
+    private String img;
+}
