f305621d20b5d5e32efb7b086cec26694267d659
Wilson <wilsonla0920@gmail.com>
2023-02-22 15:05:16 -0500

    Implemented Checkout entity

---
+++ src/main/java/com/luv2code/springbootlibrary/entity/Checkout.java
@@ -0,0 +1,33 @@
+package com.luv2code.springbootlibrary.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "checkout")
+@Data
+public class Checkout {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "userEmail")
+    private String userEmail;
+    @Column(name = "checkout_date")
+    private String checkoutDate;
+    @Column(name = "return_date")
+    private String returnDate;
+    @Column(name = "book_id")
+    private Long bookId;
+
+    public Checkout(){
+
+    }
+
+    public Checkout(String userEmail, String checkoutDate, String returnDate, Long bookId){
+        this.userEmail = userEmail;
+        this.checkoutDate = checkoutDate;
+        this.returnDate = returnDate;
+        this.bookId = bookId;
+    }
+}
