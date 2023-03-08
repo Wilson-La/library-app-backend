7c4e514738636e9d14196c5ad7100480770d2ed7
Wilson <wilsonla0920@gmail.com>
2023-03-08 18:24:46 -0500

    Implemented GET API to get a boolean result to see if user has a review posted already for a book

--- src/main/java/com/luv2code/springbootlibrary/service/ReviewService.java
+++ src/main/java/com/luv2code/springbootlibrary/service/ReviewService.java
@@ -15,13 +15,10 @@ import java.time.LocalDate;
 @Transactional
 public class ReviewService {
 
-    private BookRepository bookRepository;
-
     private ReviewRepository reviewRepository;
 
     @Autowired
-    public ReviewService(BookRepository bookRepository, ReviewRepository reviewRepository) {
-        this.bookRepository = bookRepository;
+    public ReviewService(ReviewRepository reviewRepository) {
         this.reviewRepository = reviewRepository;
     }
 
@@ -42,6 +39,16 @@ public class ReviewService {
         }
         review.setDate(Date.valueOf(LocalDate.now()));
         reviewRepository.save(review);
+    }
+
+    public Boolean userReviewListed(String userEmail, Long bookId) {
+        Review validateReview = reviewRepository.findByUserEmailAndBookId(userEmail, bookId);
+        if (validateReview != null) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
 }
