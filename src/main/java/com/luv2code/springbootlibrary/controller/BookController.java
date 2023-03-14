b0f5450d954e47f91d75d27d27eb6f2c828c41b5
Wilson <wilsonla0920@gmail.com>
2023-03-14 15:27:18 -0400

    Added Return book end point to our BookService and BookController class

--- src/main/java/com/luv2code/springbootlibrary/controller/BookController.java
+++ src/main/java/com/luv2code/springbootlibrary/controller/BookController.java
@@ -46,4 +46,11 @@ public class BookController {
         String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.currentLoansCount(userEmail);
     }
+
+    @PutMapping("/secure/return")
+    public void returnBook(@RequestHeader(value = "Authorization") String token,
+                           @RequestParam Long bookId) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        bookService.returnBook(userEmail, bookId);
+    }
 }
