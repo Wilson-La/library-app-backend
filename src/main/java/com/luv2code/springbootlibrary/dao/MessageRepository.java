776cfc614889708d5b032865992af56d132e5446
Wilson <wilsonla0920@gmail.com>
2023-03-16 17:19:33 -0400

    Implemented a method in MessageRepository to find messages posted by user and also added config for messages in MyDataRestConfig

--- src/main/java/com/luv2code/springbootlibrary/dao/MessageRepository.java
+++ src/main/java/com/luv2code/springbootlibrary/dao/MessageRepository.java
@@ -1,7 +1,14 @@
 package com.luv2code.springbootlibrary.dao;
 
 import com.luv2code.springbootlibrary.entity.Message;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.awt.print.Pageable;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
+
+    Page<Message> findByUserEmail(@RequestParam("user_email") String userEmail, Pageable pageable);
+
 }
