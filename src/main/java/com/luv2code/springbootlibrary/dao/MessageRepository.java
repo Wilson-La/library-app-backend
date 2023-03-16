8baecc0f5c590b91cdab000a4ceddd6389d7e7cc
Wilson <wilsonla0920@gmail.com>
2023-03-16 19:51:47 -0400

    Imported wrong Pageable package in MessageRepository that caused spring boot errors

--- src/main/java/com/luv2code/springbootlibrary/dao/MessageRepository.java
+++ src/main/java/com/luv2code/springbootlibrary/dao/MessageRepository.java
@@ -2,10 +2,9 @@ package com.luv2code.springbootlibrary.dao;
 
 import com.luv2code.springbootlibrary.entity.Message;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.awt.print.Pageable;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
