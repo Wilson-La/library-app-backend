0c26f36ced30b11f690eafdb1279a2b4c23b463b
Wilson <wilsonla0920@gmail.com>
2023-03-21 18:05:08 -0400

    Implemented 2 PUT endpoints to increase and decrease book quantity endpoints, and also implemented a DELETE endpoint to delete a book by bookId.

--- src/main/java/com/luv2code/springbootlibrary/controller/AdminController.java
+++ src/main/java/com/luv2code/springbootlibrary/controller/AdminController.java
@@ -18,6 +18,26 @@ public class AdminController {
         this.adminService = adminService;
     }
 
+    @PutMapping("/secure/increase/book/quantity")
+    public void increaseBookQuantity(@RequestHeader(value = "Authorization") String token,
+                                     @RequestParam Long bookId) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")){
+            throw new Exception("Administration page only");
+        }
+        adminService.increaseBookQuantity(bookId);
+    }
+
+    @PutMapping("/secure/decrease/book/quantity")
+    public void decreaseBookQuantity(@RequestHeader(value = "Authorization") String token,
+                                     @RequestParam Long bookId) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")){
+            throw new Exception("Administration page only");
+        }
+        adminService.decreaseBookQuantity(bookId);
+    }
+
     @PostMapping("/secure/add/book")
     public void postBook(@RequestHeader(value = "Authorization") String token,
                          @RequestBody AddBookRequest addBookRequest) throws Exception {
@@ -26,6 +46,16 @@ public class AdminController {
             throw new Exception("Administration page only.");
         }
         adminService.postBook(addBookRequest);
+    }
+
+    @DeleteMapping("/secure/delete/book")
+    public void deleteBook(@RequestHeader(value = "Authorization") String token,
+                           @RequestParam Long bookId) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only.");
+        }
+        adminService.deleteBook(bookId);
     }
 
 }
